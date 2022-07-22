c028592934a3c8744f052d86f5a4ca3e22a84771
PKmasai <prakashorious@gmail.com>
2022-07-22 19:59:51 +0530

    my final payment

--- my project3/payment.js
+++ my project3/payment.js
@@ -1,25 +1,18 @@
 function validation(){
 	
-	swal({
-		title: "Are you sure?",
-		text: "Once Payment, you will not be able to recover your Money!",
-		icon: "warning",
-		buttons: true,
-		dangerMode: true,
-	  })
-	  .then((willDelete) => {
-		if (willDelete) {
-		  swal("Poof! Your Payment is Sucessfull", {
-			icon: "success",
-		  });
-		} else {
-		  swal("Your payment is fullysafe!");
-		}
-		
-	
-	  });
+	// swal("Click on either the button or outside the modal.")
+	swal("Good job!", "Your Payment is Successfull!", "success")
+.then((value) => {
+  
+  window.location.reload(value)
+});
 
+
+	
+	
+	  
 	  
 	
 	
 }
+
