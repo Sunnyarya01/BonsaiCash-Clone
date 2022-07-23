e25130995704c91fa3ac3600ad4174e9a3046b65
Anurag-shekawat <anuragshekhawat806@gmail.com>
2022-07-23 10:56:09 +0530

    payment page changes done

--- my project3/payment.js
+++ my project3/payment.js
@@ -1,18 +1,6 @@
-function validation(){
-	
-	// swal("Click on either the button or outside the modal.")
-	swal("Good job!", "Your Payment is Successfull!", "success")
-.then((value) => {
-  
-  window.location.reload(value)
-});
-
-
-	
-	
-	  
-	  
-	
-	
+function validation() {
+  // swal("Click on either the button or outside the modal.")
+  swal("Good job!", "Your Payment is Successfull!", "success").then((value) => {
+    window.location.href = "../index.html";
+  });
 }
-
